57cd700e0c09c0f50962f4862fa21ecf28d1f9e8
Luiz Philipe <75584279+luizskyline@users.noreply.github.com>
2024-04-05 21:42:03 -0300

    Add files via upload

---
+++ LINKTREE CLONE NEXT.JS, REACT, TAILWIND CSS/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+        <title>Linktree</title>
+        <meta name="description" content="Next.js + Tailwind CSS" />
+        <link rel="icon" href="favicon.ico" />
+        </>
+    );
+}
